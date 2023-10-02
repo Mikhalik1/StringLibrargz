e91364bf961696f8526f8e7aef92fa6fb5c48d39
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-02 14:59:02 +0500

    Добавьте файлы проекта.

---
+++ StringCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibrargz
+{
+    public class StringCheck
+    {
+        /// <summary>
+        /// В качестве параметра передается строка, которая может быть пустой или содержать некоторый текст. Текст может включать в себя произвольные символы.
+        /// </summary>
+        /// <param name="textString"></param>
+        /// <returns>
+        /// Возвращается список символов List<char>. Список должен содержать буквы искомого текста и удовлетворять требованиям, указанным выше.
+        /// </returns>
+        public static List<char> GetLetters(string textString)
+        {
+            List<char> list = new List<char>();
+
+            string letters = new string(textString.Where(Char.IsLetter).ToArray());
+
+            char[] chars = letters.ToCharArray();
+
+            foreach (char item in chars)
+            {
+                list.Add(Char.ToUpper(item));
+            }
+
+            list = list.Distinct().ToList();
+
+            list.Sort();
+
+
+            return list;
+        }
+
+
+
+
+
+    }
+}
